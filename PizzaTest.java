4bfec5c171cab30b9be7cb27e124d648d255e0da
Ispasescu Bogdan <ispasescubogdan20@stud.ase.ro>
2024-12-15 11:33:58 +0200

    primul assignment

--- PizzaTest.java
+++ PizzaTest.java
@@ -1,23 +1,27 @@
 // PizzaTest.java
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.lang.IllegalArgumentException; 
 
 public class PizzaTest {
 
-    @Test
+   @Test
     public void testCalculatePricePerInch() {
-        Pizza pizza = new Pizza("Pepperoni", 12, 18.0);
-        assertEquals(1.5, pizza.calculatePricePerInch());
+    Pizza pizza = new Pizza("Pepperoni", 12, 18.0);
+    assertEquals(1.5, pizza.calculatePricePerInch());
 
-        Pizza smallPizza = new Pizza("Cheese", 8, 12.0);
-        assertEquals(1.5, smallPizza.calculatePricePerInch());
+    Pizza smallPizza = new Pizza("Cheese", 8, 12.0);
+    assertEquals(1.5, smallPizza.calculatePricePerInch());
 
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            Pizza invalidPizza = new Pizza("Veggie", 0, 10.0);
-            invalidPizza.calculatePricePerInch();
-        });
-        assertEquals("Size must be greater than 0", exception.getMessage());
+    try {
+        Pizza invalidPizza = new Pizza("Veggie", 0, 10.0);
+        invalidPizza.calculatePricePerInch();
+        fail("Expected IllegalArgumentException for size <= 0");
+    } catch (IllegalArgumentException e) {
+        assertEquals("Size must be greater than 0", e.getMessage());
     }
+}
+
 
     @Test
     public void testGettersAndSetters() {
